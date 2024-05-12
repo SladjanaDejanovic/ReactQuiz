b886e21a75b5c0c5c1e2d91771dc80152628433a
Sladjana <csl.bunny.2207@gmail.com>
2024-05-12 20:23:10 +0200

    finish quiz

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,7 @@
 function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to rReact Quiz!</h2>
+      <h2>Welcome to React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
